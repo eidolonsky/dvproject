093af9cbacc420429e4bc8145815f7e60dbd83f2
TianJiang <riersy2324@gmail.com>
2017-03-18 23:06:26 -0500

    add name/film csv; multi line time series

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,3 +97,83 @@ function geoGlobe(pointColor, csvGeo) {
     })
   })
 }
+function timeSeries(csvTime) {
+  //svg
+  var svg = d3.select("svg"),
+      margin = {top: 20, right: 80, bottom: 30, left: 50},
+      width = svg.attr("width") - margin.left - margin.right,
+      height = svg.attr("height") - margin.top - margin.bottom,
+      g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  //parse time
+  var parseTime = d3.timeParse("%H:%M");
+  //scale
+  var x = d3.scaleTime().range([0, width]),
+      y = d3.scaleLinear().range([height, 0]),
+      z = d3.scaleOrdinal(d3.schemeCategory10);
+  //line
+  var line = d3.line()
+      .curve(d3.curveBasis)
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.sum); });
+  //get csv data
+  d3.csv(csvTime, type, function(error, data) {
+    if (error) throw error;
+
+    var names = data.columns.slice(1).map(function(id) {
+      return {
+        id: id,
+        values: data.map(function(d) {
+          return {date: d.date, sum: d[id]};
+        })
+      };
+    });
+    //domain
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+
+    y.domain([
+      d3.min(names, function(c) { return d3.min(c.values, function(d) { return d.sum; }); }),
+      d3.max(names, function(c) { return d3.max(c.values, function(d) { return d.sum; }); })
+    ]);
+
+    z.domain(names.map(function(c) { return c.id; }));
+    //axis
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("fill", "#000")
+        .text("sum, ºF");
+
+    var name = g.selectAll(".name")
+      .data(names)
+      .enter().append("g")
+        .attr("class", "name");
+
+    name.append("path")
+        .attr("class", "line")
+        .attr("d", function(d) { return line(d.values); })
+        .style("stroke", function(d) { return z(d.id); });
+
+    name.append("text")
+        .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.sum) + ")"; })
+        .attr("x", 3)
+        .attr("dy", "0.35em")
+        .style("font", "10px sans-serif")
+        .text(function(d) { return d.id; });
+  });
+
+  function type(d, _, columns) {
+    d.date = parseTime(d.date);
+    for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
+    return d;
+  }
+}
