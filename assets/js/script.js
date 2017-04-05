63d7676885a5ce611e9ac279ef912262e4b3ff66
TianJiang <riersy2324@gmail.com>
2017-04-05 16:03:30 -0500

    remove nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,5 @@
 //
 //
-//---------------------------------   NavBar Start   ---------------------------------
-//
-//
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-//
-//
-//---------------------------------   NavBar End   ---------------------------------
-//
-//
-
-//
-//
 //---------------------------------   GeoGlobe Start   ---------------------------------
 //
 //
